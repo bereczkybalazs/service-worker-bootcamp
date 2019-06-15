19ede97d5122e56bb44ea60b03f455253a65db22
Bereczky Balazs <bbereczky@hearsaycorp.com>
2019-06-15 15:05:26 +0200

    store files in cache

--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,8 +1,32 @@
+const cacheName = 'v1'
+
+
+//cache manually
+const cacheAssets = [
+    'index.html',
+    'yoshi.html',
+    '/css/style.css',
+    '/js/main.js'
+]
+
+
 self.addEventListener('install', (event) => {
     console.log('service worker installed');
+    event.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                console.log('service worker: caching files');
+                cache.addAll(cacheAssets)
+            })
+            .then(() => {
+                self.skipWaiting()
+            })
+    )
 })
 
 //here you can clean old cache here
 self.addEventListener('activate', (event) => {
     console.log('service worker activated');
+
 })
